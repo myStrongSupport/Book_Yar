613c2dc71ec7c38a886de2f5447d3b39c5ef152d
Hamed <hamedroaddark@gmail.com>
2024-10-15 14:56:39 +0330

    initailize_tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          50: "hsl(from #daaa63 h s l / 0.2)",
+          70: "hsl(from #daaa63 h s l / 0.3)",
+          100: "#f8f3ed",
+          200: "#daaa63",
+          300: "#e5830d",
+          400: "#967349",
+          500: "#7a4f37",
+          600: "#220e07",
+        },
       },
     },
   },
