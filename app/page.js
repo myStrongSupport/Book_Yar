491029b338f671259f4895e2027d719d9959c2f4
Hamed <hamedroaddark@gmail.com>
2024-10-15 15:54:44 +0330

    add_Reading_list_to_homePage

--- app/page.js
+++ app/page.js
@@ -1,6 +1,16 @@
 import Hero from "@/components/Home/Hero/Hero";
+import BookState from "@/components/BookState/BookState";
+async function getBooks() {
+  const res = await fetch(`/api/booksBeingRead`, {
+    cache: "no-store",
+  });
+
+  return await res.json();
+}
 
 export default async function Home() {
+  const readingBooks = await getBooks();
+
   return (
     <>
       <section className="md:h-[87dvh]">
@@ -10,7 +20,9 @@ export default async function Home() {
       </section>
       {/* Book is being Read */}
       <section className="overflow-hidden py-10">
-        <div className="m-auto px-1 xl:container"></div>
+        <div className="m-auto px-1 xl:container">
+          <BookState state="کتاب های درحال مطالعه" books={readingBooks} />
+        </div>
       </section>
     </>
   );
