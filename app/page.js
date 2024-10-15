eeac915398f95c9321f1f4dd2042c795d73f741f
Hamed <hamedroaddark@gmail.com>
2024-10-15 17:01:14 +0330

    add_homepage

--- app/page.js
+++ app/page.js
@@ -1,22 +1,43 @@
-import Hero from "@/components/Home/Hero/Hero";
 import BookState from "@/components/BookState/BookState";
+import Hero from "@/components/Home/Hero/Hero";
+import ShowChallange from "@/components/Home/ShowChallange/ShowChallange";
+import Loading from "@/components/Loading/loading";
+import { Suspense } from "react";
 
 export default async function Home() {
-  const data = await fetch(
-    "https://book-yar-shar.vercel.app/api/booksBeingRead"
+  const dataReadingBooks = await fetch(
+    "https://book-yar-shar.vercel.app//api/booksBeingRead"
   );
-  const readingBooks = await data.json();
+  const dataChallangeBooks = await fetch(
+    "https://book-yar-shar.vercel.app//api/booksBeingRead"
+  );
+  const challangeBooks = await dataChallangeBooks.json();
+  const readingBooks = await dataReadingBooks.json();
+  const filteredReadingBooks = readingBooks.filter(
+    (book) => book.isReading === true
+  );
   return (
     <>
       <section className="md:h-[87dvh]">
         <div className="container relative m-auto h-auto px-8 md:h-full">
-          <Hero />
+          <Suspense fallback={<Loading />}>
+            {challangeBooks.length === 0 ? (
+              <Hero />
+            ) : (
+              <ShowChallange books={challangeBooks} />
+            )}
+          </Suspense>
         </div>
       </section>
       {/* Book is being Read */}
       <section className="overflow-hidden py-10">
         <div className="m-auto px-1 xl:container">
-          <BookState state="کتاب های درحال مطالعه" books={readingBooks} />
+          <Suspense fallback={<Loading />}>
+            <BookState
+              state="کتاب های درحال مطالعه"
+              books={filteredReadingBooks}
+            />
+          </Suspense>
         </div>
       </section>
     </>
