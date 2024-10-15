217ad667b00ae711cfe40cee140b235b2bbf9610
Hamed <hamedroaddark@gmail.com>
2024-10-15 15:50:01 +0330

    add_hero_to_homepage

--- app/page.js
+++ app/page.js
@@ -1,5 +1,17 @@
-import Image from "next/image";
+import Hero from "@/components/Home/Hero/Hero";
 
-export default function Home() {
-  return <div></div>;
+export default async function Home() {
+  return (
+    <>
+      <section className="md:h-[87dvh]">
+        <div className="container relative m-auto h-auto px-8 md:h-full">
+          <Hero />
+        </div>
+      </section>
+      {/* Book is being Read */}
+      <section className="overflow-hidden py-10">
+        <div className="m-auto px-1 xl:container"></div>
+      </section>
+    </>
+  );
 }
